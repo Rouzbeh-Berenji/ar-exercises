6c8cb10eece2e773be9668d0a3f8a79311fa0d1e
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-12-15 16:28:46 -0500

    completing LHL exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,26 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+
+  @store4 = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true )
+  
+  @store5 = Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false )
+  
+  @store6 = Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true )
+  
+  @mens_stores = Store.where(mens_apparel: true)
+
+  puts "all mens stores"
+
+    @mens_stores.each do |elem|
+      p elem.name
+    end
+
+    puts "Womens stores selling under 1 million"
+
+  @under_mil_womens = Store.where("annual_revenue < 1000000").where(womens_apparel: true)
+
+    @under_mil_womens.each do |elem|
+      p elem.name
+    end
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+
+puts "Please enter a new store name"
+new_store_name = gets.chomp
+
+
+@new_store = Store.create(name: new_store_name, annual_revenue: nil, mens_apparel: nil, womens_apparel: nil )
+
+puts @new_store.errors.full_messages
